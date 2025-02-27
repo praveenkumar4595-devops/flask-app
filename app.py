2721fc6af73ebdb8218dc74fbe0dbb284a032299
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-02-27 12:57:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask App running on Docker!"
+    return "Hello Siva, Flask App running on K8's using Harness!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
