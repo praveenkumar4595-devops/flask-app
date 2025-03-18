f8c9d0a6a0ac9591bfbc399844f86d4b0a6b4331
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-03-18 14:27:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Venky, Flask App running on K8's using Harness!"
+    return "Hello Sekhar, Flask App running on K8's using Harness!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
